0d59ed768798d866b137c6e196217a3972e64abb
aihacker <271671891@qq.com>
2020-05-14 11:33:57 +0800

    可用
    代码来源：[原创]记一次frida实战——对某视频APP的脱壳、hook破解、模拟抓包、协议分析一条龙服务
    代码链接：https://bbs.pediy.com/thread-258776.htm
    可用状态：可以直接使用

---
+++ list_libartso_exports.py
@@ -0,0 +1,23 @@
+import frida
+import sys
+
+jscode = """
+
+  var exports = Module.enumerateExportsSync("libart.so");
+    for(var i=0;i<exports.length;i++){
+        send("name:"+exports[i].name+"  address:"+exports[i].address);
+     }
+
+"""
+
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print("[*] {0}".format(message['payload']))
+
+
+process = frida.get_usb_device().attach("com.cz.babySister")
+script = process.create_script(jscode)
+script.on('message', on_message)
+script.load()
+sys.stdin.read()
